71501ebfc90ed76841b0b4e94257ace2163018f6
francknemeghaire <franck.nemeghaire@live.be>
2024-05-12 07:22:12 +0200

    mise au point du code pour utilisation de tous les nouveaux champs - en cours - à tester

--- package/requetesql.py
+++ package/requetesql.py
@@ -1,53 +1,34 @@
 import sqlite3
-"""champs à ajouter pour mise à jour bdd
-    nom = models.CharField(max_length=200)
-    envergure = models.IntegerField(default=0)
-    hauteur = models.IntegerField(default=0)
-    exposition = models.CharField(max_length=300)
-    datedesemis = models.DateField()
-    datedeplantation = models.DateField()
-    dureedevie = models.IntegerField(default=0)
-    typearrosage = models.CharField(max_length=200)
-    typedesol = models.CharField(max_length=300)
-    association = models.CharField(max_length=300)
-    temperaturegermination = models.IntegerField()
-    typedeplante = models.CharField(max_length=200)
-    couleur = models.CharField(max_length=200)
-    emplacement = models.CharField(max_length=300)
-    feuillagepersistant = models.BooleanField(default=False)
-    mellifere = models.BooleanField(default=False)
-    moisdefloraison = models.CharField(max_length=50)
-    moisderecolte = models.CharField(max_length=50)
-    planteparfumee = models.BooleanField(default=False)
-    plantevivace = models.BooleanField(default=False)
-"""
+
 def maj_bdd(tampon):
     """ajout à la bdd"""
     connection = sqlite3.connect("plantes.db")
     cursor = connection.cursor()
-    cursor.executemany("""
-    INSERT INTO plantes (
-    id,
-    nom,
-    envergure,
-    hauteur,
-    exposition,
-    datedesemis,
-    datedeplantation,
-    dureedevie,
-    typearrosage,
-    typedesol,
-    association,
-    temperaturegermination,
-    typedeplante,
-    couleur,
-    emplacement,
-    feuillagepersistant,
-    mellifere,
-    moisdefloraison,`
-    moisderecolte,
-    planteparfumee,
-    plantevivace) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?)""", tampon)
+    requetesql = "insert into plantes (nom, envergure, hauteur, exposition, datedesemis, datedeplantation, dureedevie, typearrosage, typedesol, association, temperaturegermination, typedeplante, couleur, emplacement, feuillagepersistant, mellifere, moisdefloraison, moisderecolte, planteparfumee, plantevivace) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?)"
+    cursor.executemany(requetesql, tampon)
+    # cursor.executemany("""
+    # INSERT INTO plantes (
+    # id,
+    # nom,
+    # envergure,
+    # hauteur,
+    # exposition,
+    # datedesemis,
+    # datedeplantation,
+    # dureedevie,
+    # typearrosage,
+    # typedesol,
+    # association,
+    # temperaturegermination,
+    # typedeplante,
+    # couleur,
+    # emplacement,
+    # feuillagepersistant,
+    # mellifere,
+    # moisdefloraison,`
+    # moisderecolte,
+    # planteparfumee,
+    # plantevivace) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?)""", tampon)"""
     connection.commit()
     connection.close()
 
