3983456018e7dc88bd4addf3e8a2b60bab4d7585
FranckNemeghaire <105145441+francknemeghaire@users.noreply.github.com>
2024-11-11 12:16:45 +0100

    Add files via upload
    factorisation, identification des problèmes dans l'appli, ajout de plante à déplacer dans subapplication pour séparer l'appli des fonctions tierces

--- package/requetesql.py
+++ package/requetesql.py
@@ -1,11 +1,10 @@
 import sqlite3
-#créer un champ photo (lien)
 
 def maj_bdd(tampon):
     """ajout à la bdd"""
     connection = sqlite3.connect("plantes.db")
     cursor = connection.cursor()
-    requetesql = "insert into plantes (nom, envergure, hauteur, exposition, datedesemi, datedeplantation, dureedevie, typearrosage, typedesol, association, temperaturegermination, typedeplante, couleur, emplacement, feuillagepersistant, mellifere, moisdefloraison, moisderecolte, planteparfumee, plantevivace) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?)"
+    requetesql = "insert into plantes (nom, envergure, hauteur, exposition, datedesemi, datedeplantation, dureedevie, typearrosage, typedesol, association, temperaturegermination, typedeplante, couleur, emplacement, feuillagepersistant, mellifere, moisdefloraison, moisderecolte, planteparfumee, plantevivace, cheminaccesimage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?)"
     cursor.executemany(requetesql, tampon)
     connection.commit()
     connection.close()
