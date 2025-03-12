0833e16da848233b2b562b1efdfbce5874086979
FranckNemeghaire <105145441+francknemeghaire@users.noreply.github.com>
2025-03-12 15:13:57 +0100

    Add files via upload
    mise à jour pour menu contextuel via affichage base de données, et modification fiche plante aussi (en cours)

--- package/requetesql.py
+++ package/requetesql.py
@@ -18,5 +18,39 @@ def tailledelabdd():
     connection.close()
     return record_count
 
+def modification_fiche(nom, envergure, hauteur, exposition, datedesemi, datedeplantation, dureedevie, typearrosage, typedesol, association, temperaturegermination, typedeplante, couleur, emplacement, feuillagepersistant, mellifere, moisdefloraison, moisderecolte, planteparfumee, plantevivace, cheminaccesimage, id_de_bdd):
+    # 21 champs
+    id = id_de_bdd
+    nom = nom
+    envergure = envergure
+    hauteur = hauteur
+    exposition = exposition
+    datedesemi = datedesemi
+    datedeplantation = datedeplantation
+    dureedevie = dureedevie
+    typearrosage = typearrosage
+    typedesol = typedesol
+    association = association
+    temperaturegermination = temperaturegermination
+    typedeplante = typedeplante
+    couleur = couleur
+    emplacement = emplacement
+    feuillagepersistant = feuillagepersistant
+    mellifere = mellifere
+    moisdefloraison = moisdefloraison
+    moisderecolte = moisderecolte
+    planteparfumee = planteparfumee
+    plantevivace = plantevivace
+    cheminaccesimage = cheminaccesimage
+
+    sql = ('UPDATE plantes SET colonne1=?, colonne2=?, colonne3=?, colonne4=?, colonne5=?, colonne7=?, colonne8=?, colonne9=?, colonne10=?, colonne11=?, colonne12=?, colonne13=?, colonne14=?, colonne15=?, colonne16=?, colonne17=?, colonne18=?, colonne19=?, colonne20=?, colonne21=?  WHERE id = ?')
+
+    try:
+        with sqlite3.connect(database) as conn:
+            cur = conn.cursor()
+            cur.execute(sql, (nom, envergure, hauteur, exposition, datedesemi, datedeplantation, dureedevie, typearrosage, typedesol, association, temperaturegermination, typedeplante, couleur, emplacement, feuillagepersistant, mellifere, moisdefloraison, moisderecolte, planteparfumee, plantevivace, cheminaccesimage, id))
+            conn.commit()
+    except sqlite3.OperationalError as e:
+        print(e)
 
 
